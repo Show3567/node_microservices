5a355d51eba726acd413576010f623000531afe1
Yue Dong <ydong011@fiu.edu>
2024-03-26 16:05:54 -0500

    update

--- auth/src/config/selectSecret.ts
+++ auth/src/config/selectSecret.ts
@@ -3,18 +3,20 @@ import path from "path";
 import "../core/env.config";
 import { getConfigData } from "./getConfig";
 
-const fileExistsInFolder = (fileName: string) => {
-	const filePath = path.join(__dirname, fileName);
+const fileExistsInFolder = (keyName: string) => {
+	const filePath = path.join(__dirname, `id_rsa_${keyName}.pem`);
 	return fs.existsSync(filePath);
 };
 
 export const selectSecret = (keyName: "priv" | "pub") => {
 	const config = getConfigData();
-	const fileName = `id_rsa_${keyName}.pem`;
-	const hasfile = fileExistsInFolder(fileName);
-	const algorithm = hasfile ? "RS256" : "HS256";
-	const secret = hasfile
-		? fs.readFileSync(path.join(__dirname, fileName), "utf-8")
+	const fileExist = fileExistsInFolder(keyName);
+	const algorithm = fileExist ? "RS256" : "HS256";
+	const secret = fileExist
+		? fs.readFileSync(
+				path.join(__dirname, `id_rsa_${keyName}.pem`),
+				"utf-8"
+		  )
 		: config.JWT_SECRET;
 
 	return { algorithm, secret };
