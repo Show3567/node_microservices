54854a5988331ee59286bffa46c078853806321c
Yue Dong <ydong011@fiu.edu>
2024-05-12 08:53:29 -0500

    update

--- config/src/apis/config.service.ts
+++ config/src/apis/config.service.ts
@@ -13,28 +13,28 @@ const getConfigData = () => {
 };
 
 export const getConfig: RequestHandler = async (req, res) => {
-	// const filePath = path.join(__dirname, "../store", "config.json");
+	const filePath = path.join(__dirname, "../store", "config.json");
 	// {data: [...]};
 	const configs = [...req.body.data];
-	const configFromGit = await fetchConfigFromGitHub("config.json");
+	// const configFromGit = await fetchConfigFromGitHub("config.json");
 
 	// console.log("data: ", JSON.parse(configFromGit));
 
-	// if (fs.existsSync(path.dirname(filePath))) {
-	// const data = getConfigData();
-	const data = JSON.parse(configFromGit);
-	const result = configs.reduce((acc, cur) => {
-		return {
-			...acc,
-			[cur]: data[cur],
-		};
-	}, {});
+	if (fs.existsSync(path.dirname(filePath))) {
+		const data = getConfigData();
+		// const data = JSON.parse(configFromGit);
+		const result = configs.reduce((acc, cur) => {
+			return {
+				...acc,
+				[cur]: data[cur],
+			};
+		}, {});
 
-	logger.info(loggerInfo("signin", 201, result));
-	res.status(201).json(result);
-	// } else {
-	// 	res.status(404).json({ message: "Cannot found config file" });
-	// }
+		logger.info(loggerInfo("getConfig", 201, result));
+		res.status(201).json(result);
+	} else {
+		res.status(404).json({ message: "Cannot found config file" });
+	}
 };
 
 export const getKey: RequestHandler = async (req, res) => {
