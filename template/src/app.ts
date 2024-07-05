82dbbfc483fb35aedf9851f8ed61f97f98f35fcc
Yue Dong <ydong011@fiu.edu>
2024-07-05 13:58:14 -0500

    update

--- template/src/app.ts
+++ template/src/app.ts
@@ -24,13 +24,17 @@ import "./core/env.config";
   & init project, install express;
   $ npm init
   $ npm i express
+
   & nodemon with ts;
   $ npm install -D nodemon typescript ts-node
+
   & edit tsconfig.json;
     "outDir": "./dist"  
+
   & add start and build in package.josn script;
   	"start": "npx nodemon --exec ts-node ./src/app.ts",
 		"build": "tsc"
+
   & dotenv to use process.env;
   $ npm install dotenv
   $ npm install --save-dev @types/dotenv
@@ -38,9 +42,11 @@ import "./core/env.config";
   & mongoose;
   $ npm install mongoose
   $ npm install --save-dev @types/mongoose
+
   & mySql;
   $ npm install typescript @types/node mysql2
   $ npm install --save-dev @types/mysql
+  
   & TypeOrm;
   $ npm install typeorm mysql reflect-metadata
   $ npm install --save-dev typescript @types/node ts-node
